017c24f48f9437dd94fdf6d7fb7fb123b6269d0a
Darmaabazar <bazaracer4@gmail.com>
2024-05-28 20:04:55 +0800

    ddddd

--- app.js
+++ app.js
@@ -2,31 +2,16 @@ const clock = document.querySelector(".clock");
 const audio = document.querySelector(".audio")
 
 
-function disableAutoplay() { 
-    audio.autoplay = false;
-    audio.load();
-}
-
 var currentTime = setInterval(function(){
 	var date = new Date();
 	
-	var hours = (12 - (date.getHours()));
+	var hours = date.getHours();
 	
 	var minutes = date.getMinutes();
 	
 	var seconds = date.getSeconds();
 	
-	var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
-
-	if (hours < 0) {
-		hours = hours * -1;
-	} else if (hours == 0) {
-		hours = 12;
-	} else {
-		hours = hours;
-	}
-	
-	clock.textContent = addZero(hours) + " : " + addZero(minutes) + " : " + addZero(seconds) + " " + ampm;
+	clock.textContent = addZero(hours) + " : " + addZero(minutes) + " : " + addZero(seconds);
 	
 }, 1000);
 
@@ -41,10 +26,16 @@ var audioPlay = setInterval(function(){
 	
 	var minutes = date.getMinutes();
 	
-	if(minutes === 0 || minutes === 30){
+	if((minutes >= 0 && minutes <= 3) || (minutes >= 30 && minutes <= 33)){
         audio.autoplay = true;
-        audio.load();
+		audio.load();
     }
 	
-}, 60000);
+}, 180000);
+
+
+function disableAutoplay() { 
+    audio.autoplay = false;
+    audio.load();
+}
 
